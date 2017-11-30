1e04be812b8a63028e32600a1bf4f1200ea8cf5a
Shaun Haber <srhaber@gmail.com>
2017-11-29 16:21:11 -0800

    Change import path

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/hashicorp/terraform/plugin"
-	"github.com/terraform-providers/terraform-provider-kubernetes/kubernetes"
+	"github.com/lumoslabs/terraform-provider-kubernetes/kubernetes"
 )
 
 func main() {
